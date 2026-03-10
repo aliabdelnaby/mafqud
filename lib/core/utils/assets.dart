5ea14654a9937753e494ec12a82a95c174689fc1
Ali Abd Elnaby <aliabdelnabi239@gmail.com>
2026-03-10 21:30:23 +0200

    Add new SVG icons for password and Twitter, and update asset utility class
    - Added password_icon.svg and twitter_icon.svg to the assets/images directory.
    - Updated the Assets utility class to include constants for the new icons.

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,41 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesEmailLogo
+  /// assets/images/email_logo.svg
+  static const String imagesEmailLogo = "assets/images/email_logo.svg";
+
+  /// Assets for imagesFacebookIcon
+  /// assets/images/facebook_icon.svg
+  static const String imagesFacebookIcon = "assets/images/facebook_icon.svg";
+
+  /// Assets for imagesInstagramIcon
+  /// assets/images/instagram_icon.svg
+  static const String imagesInstagramIcon = "assets/images/instagram_icon.svg";
+
+  /// Assets for imagesMafqudAppbar
+  /// assets/images/mafqud_appbar.svg
+  static const String imagesMafqudAppbar = "assets/images/mafqud_appbar.svg";
+
+  /// Assets for imagesMafqudAuthLogo
+  /// assets/images/mafqud_auth_logo.svg
+  static const String imagesMafqudAuthLogo = "assets/images/mafqud_auth_logo.svg";
+
+  /// Assets for imagesMissingPersonImage
+  /// assets/images/missing_person_image.png
+  static const String imagesMissingPersonImage = "assets/images/missing_person_image.png";
+
+  /// Assets for imagesOnBoardingImage
+  /// assets/images/onBoardingImage.png
+  static const String imagesOnBoardingImage = "assets/images/onBoardingImage.png";
+
+  /// Assets for imagesPasswordIcon
+  /// assets/images/password_icon.svg
+  static const String imagesPasswordIcon = "assets/images/password_icon.svg";
+
+  /// Assets for imagesTwitterIcon
+  /// assets/images/twitter_icon.svg
+  static const String imagesTwitterIcon = "assets/images/twitter_icon.svg";
+}
+
