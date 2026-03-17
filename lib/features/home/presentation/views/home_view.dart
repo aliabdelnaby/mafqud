0287960faef199070ca1224aead62a5ea1c01185
Ali Abd Elnaby <aliabdelnabi239@gmail.com>
2026-03-17 16:49:15 +0200

    Add AppBar and Drawer to HomeView for improved navigation and branding

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:mafqud/core/utils/assets.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -6,6 +8,8 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: SvgPicture.asset(Assets.imagesMafqudAppbar)),
+      endDrawer: Drawer(),
       body: Center(
         child: Text(
           "Home View",
