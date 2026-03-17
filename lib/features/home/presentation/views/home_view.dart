cbc96c9b3797e9b97e2c023e04cca11f09d3facb
Ali Abd Elnaby <aliabdelnabi239@gmail.com>
2026-03-17 17:24:10 +0200

    Add FilterByGovernorateWidget for improved filtering options in HomeView

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:mafqud/core/utils/assets.dart';
+import 'package:mafqud/features/home/presentation/widgets/filter_by_governorate_widget.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -10,11 +11,9 @@ class HomeView extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: SvgPicture.asset(Assets.imagesMafqudAppbar)),
       endDrawer: Drawer(),
-      body: Center(
-        child: Text(
-          "Home View",
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
-        ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(children: [FilterByGovernorateWidget()]),
       ),
     );
   }
---
+++ lib/features/home/presentation/widgets/filter_by_governorate_widget.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:mafqud/core/data/datasource/list_of_governarets.dart';
+import 'package:mafqud/core/utils/app_colors.dart';
+
+class FilterByGovernorateWidget extends StatefulWidget {
+  const FilterByGovernorateWidget({super.key});
+
+  @override
+  State<FilterByGovernorateWidget> createState() =>
+      _FilterByGovernorateWidgetState();
+}
+
+class _FilterByGovernorateWidgetState extends State<FilterByGovernorateWidget> {
+  String? selectedGovernorate;
+  @override
+  Widget build(BuildContext context) {
+    return SearchBar(
+      hintText: selectedGovernorate ?? "Filter by governorate",
+      hintStyle: WidgetStateProperty.resolveWith(
+        (states) => TextStyle(
+          color: Color(0xff333333).withValues(alpha: 0.87),
+          fontSize: 15,
+          fontWeight: FontWeight.w400,
+          letterSpacing: 0.2,
+        ),
+      ),
+      trailing: [Icon(Icons.filter_list_outlined, color: Colors.grey.shade600)],
+      backgroundColor: WidgetStateProperty.resolveWith(
+        (states) => Colors.white,
+      ),
+      shape: WidgetStateProperty.resolveWith(
+        (states) => RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(22),
+          side: BorderSide(color: AppColors.primaryColor),
+        ),
+      ),
+      elevation: WidgetStateProperty.resolveWith((states) => 0),
+      readOnly: true,
+      onTap: () {
+        showModalBottomSheet(
+          context: context,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+          ),
+          builder: (context) {
+            return ListView.builder(
+              padding: EdgeInsets.all(16),
+              itemCount: governorates.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(governorates[index]),
+                  onTap: () {
+                    setState(() {
+                      selectedGovernorate = governorates[index];
+                    });
+                    if (kDebugMode) {
+                      print(governorates[index]);
+                    }
+                    context.pop();
+                  },
+                );
+              },
+            );
+          },
+        );
+      },
+    );
+  }
+}
